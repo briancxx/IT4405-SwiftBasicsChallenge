b925c8bb59510aef921c004e36f00beadacd2177
Brian Cox <briancxx@icloud.com>
2019-02-08 13:07:01 -0600

    First commit

---
+++ Swift Basics Challenge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics Challenge
+//
+//  Created by Brian Cox on 2/8/19.
+//  Copyright © 2019 Brian Cox. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
